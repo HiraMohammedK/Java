28984917179fcffe61c636b73812c2feda8bab68
HiraMohammedK <98226271+HiraMohammedK@users.noreply.github.com>
2022-05-20 15:17:26 +0530

    Add files via upload

---
+++ SumGivenNo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumGivenNo
+{
+	public static void main(String[] args) {
+	   int a=5;
+	   int b=6;
+	    
+	    int sum;
+	    
+	    sum=a+b;
+		System.out.println("Sum= "+sum);
+	
+	}
+}
+
