e1fa92ff37aec70e7366d3a8cda6297816d042f6
HiraMohammedK <98226271+HiraMohammedK@users.noreply.github.com>
2026-05-06 15:56:02 +0530

    Add binary search implementation in Java
    Implement binary search algorithm to find a target in a sorted array.

---
+++ DSA/binary_search.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int left = 0;
+        int right = n - 1;
+        boolean found = false;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(arr[mid] == target) {
+                found = true;
+                break;
+            }
+            else if(arr[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        if(found) {
+            System.out.println("Found");
+        }
+        else {
+            System.out.println("Not Found");
+        }
+    }
+}
